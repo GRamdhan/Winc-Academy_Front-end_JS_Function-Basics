233bc580566858e5a43dbcc2276a76a7e787d735
GRamdhan <73478034+GRamdhan@users.noreply.github.com>
2021-11-23 14:24:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Make the dough");
+const makePizza = function () {
+    console.log("Roll out the dough");
+    console.log("Put the toppings on the dough");
+    console.log("Put the pizza in the oven");
+};
+/*for (var i = 0; i < 3; i++) {
+    makePizza();
+}
+*/
+const makeSushi = function () {
+    console.log("Spread prepared sushi rice on the nori");
+    console.log("Put toppings on the rice");
+    console.log("Roll the sushi and cut in pieces")
+};
+for (var i = 0; i < 3; i++) {
+    makeSushi();
+}
+for (var i = 0; i < 2; i++) {
+    makePizza();
+}
